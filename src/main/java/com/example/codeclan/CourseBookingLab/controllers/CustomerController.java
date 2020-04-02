5ed1670b3e3b718e43e5e605a4eb5d56695e372d
user <blah@blah.blah>
2020-04-02 14:37:20 +0100

    extensions complete

--- src/main/java/com/example/codeclan/CourseBookingLab/controllers/CustomerController.java
+++ src/main/java/com/example/codeclan/CourseBookingLab/controllers/CustomerController.java
@@ -15,9 +15,20 @@ public class CustomerController {
     CustomerRepo customerRepo;
     @GetMapping(value="customers")
     public ResponseEntity getAllCustomers(
-            @RequestParam(required = false, name="courseName")String courseName
+            @RequestParam(required = false, name="courseName")String courseName,
+            @RequestParam(required = false, name="town")String town,
+            @RequestParam(required = false, name="ageGreaterThan")Integer ageGreaterThan
     ){
+        if(ageGreaterThan!=null && courseName!=null && town!=null){
+            //http://localhost:8080/customers?town=Largs&courseName=Data%20Analysis&ageGreaterThan=40
+            return new ResponseEntity(customerRepo.findCustomerByTownAndBookingsCourseNameAndAgeGreaterThan(town, courseName, ageGreaterThan), HttpStatus.OK);
+        }
+        if(courseName!=null && town!=null){
+            //http://localhost:8080/customers?town=Largs&courseName=Data%20Analysis
+            return new ResponseEntity(customerRepo.findCustomerByTownAndBookingsCourseName(town, courseName), HttpStatus.OK);
+        }
         if (courseName != null){
+            http://localhost:8080/customers?courseName=Data%20Analysis
             return new ResponseEntity(customerRepo.findCustomerByBookingsCourseName(courseName), HttpStatus.OK);
         }
         return new ResponseEntity(customerRepo.findAll(), HttpStatus.OK);
